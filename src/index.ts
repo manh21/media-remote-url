0ccc9430e42852f2c6edf1669ab241d535d2bca5
Naufal Hakim <31276236+manh21@users.noreply.github.com>
2024-06-12 11:13:24 +0700

    initial code

--- src/index.ts
+++ src/index.ts
@@ -10,4 +10,35 @@ app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
 
+app.post('/save', async (c) => {
+  const body = await c.req.parseBody();
+
+  // Check if downloadUrl is a valid URL
+  const downloadUrlString = body['remote_url'] as string;
+  if (!downloadUrlString) {
+    return c.text('Invalid URL')
+  }
+
+  if (!downloadUrlString.startsWith('http') || !downloadUrlString.includes('://')) {
+    return c.text('Invalid URL')
+  }
+
+  // Check filename
+  let filename = body['filename'] as string;
+  if (!filename) {
+    // Generate a random filename
+    filename = Math.random().toString(36).substring(7);
+  }
+
+  // Fetch the file from the URL
+  let { headers, body: stream } = await fetch(downloadUrlString)
+
+  await c.env.HONO_R2_UPLOAD.put(filename, stream);
+  console.log(`Uploaded ${filename} to R2 Complete!`)
+
+  return c.json({
+    filename
+  });
+})
+
 export default app
